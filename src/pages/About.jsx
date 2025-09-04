25f5cad7ee429649a689af0143749cad97b3c0a2
lovishverma1435 <vermalovey5@gmail.com>
2025-09-04 10:11:20 +0530

    committ

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -33,7 +33,7 @@ const About = () => {
               </p>
               <div className=" flex button gap-4 sm:gap-6 w-full pb-16 md:pb-0">
                 <a
-                  href="https://drive.usercontent.google.com/download?id=1KN-tfEHaDdDyJct6rhCcDt0_oyUPZWpx&export=download&authuser=0&confirm=t&uuid=897c23e0-8bb3-4eee-bd5f-ea8254459639&at=AN8xHoppkLzmUb3q43PKS-mYnft_:1755671016408"
+                  href="https://drive.usercontent.google.com/download?id=1oHxtBgAOB4prO6MX03dkd3T1Xmno-LG_&export=download&authuser=0&confirm=t&uuid=40682eac-e488-4268-8508-900586cb5ba1&at=AN8xHoqK_qp0tg66Esv1c4X-19jC:1756960813085"
                   download={"cv"}
                   className='bg-[#ec5631] w-full py-3 px-14 rounded-xl flex justify-center items-center font-semibold text-lg gap-3 hover:scale-105 transition-transform duration-300 shadow-lg'
                 >
